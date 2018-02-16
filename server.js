9f45b36814623acf0c0f710e4932fec6dceba6ec
Jay Schwane <jrschwane@uwalumni.com>
2018-02-16 16:59:14 -0600

    Adds basic server entry point

---
+++ server.js
@@ -0,0 +1,29 @@
+/* ================================= SETUP ================================= */
+
+const express    = require('express');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const app        = express();
+const db         = require('./db');
+const port       = process.env.PORT || 3000;
+
+
+/* ================================ CONFIG ================================= */
+
+app.use('/users',      require('./controllers/users'));
+app.use('/portfolios', require('./controllers/portfolios'));
+
+
+/* ========================== CONNECT TO DATABASE ========================== */
+
+db.connect('mongodb://localhost:27017/stocktracker', (err) => {
+  if (err) {
+    // bail
+    console.log('Unable to connect to MongoDB');
+    process.exit(1);
+  } else {
+    // start server
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  }
+
+});
