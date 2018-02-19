1a2e75f8a07351f4de393d69bc3b9b0f60f09e06
Jay Schwane <jrschwane@uwalumni.com>
2018-02-19 00:54:09 -0600

    Completes, updates, fixes portfolios controller

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -2,50 +2,30 @@
 
 /* ================================= SETUP ================================= */
 
-const router     = require('express').Router();
-const jwt        = require('jsonwebtoken');
-const Portfolios = require('../models/portfolios');
+const router      = require('express').Router();
+const verifyJWT   = require('../middleware/verifyJWT');
+const Portfolios  = require('../models/portfolios');
+
+const isOid       = require('../utils/validateObjectIds');
+const isTicker    = require('../utils/validateTickers');
+
+const badParamMsg = 'Malformed URL paramter';
+const badBodyMsg  = 'Malformed request body parameter';
+
 
 /* ============================== MIDDLEWARE =============================== */
 
-/**
- * Verify JWT token
-*/
-const verifyJWT = (req, res, next) => {
-  const token  = req.headers.authorization;
-  const secret = process.env.JWT_SECRET;
-
-  // fail if missing required pieces
-  if (!token || !secret) {
-    return next(new Error('Missing components for JWT verification'));
-  }
-
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      return next(new Error(err.message));
-    }
-
-    if (decoded.user._id && decoded.user.username) {
-      // OK. Set req.user
-      req.user = decoded.user;
-      next();
-    } else {
-      return next(new Error('Missing required user attributes'));
-    }
-
-  });
-  
-};
-
 router.use(verifyJWT);
+
 
 /* ========================== ROUTE CONTROLLERS ============================ */
 
 /**
  * Get all user's portfolios
  * Example: GET >> /api/portfolios
- * Secured: yes
- * Expects: user's _id from valid JWT
+ * Secured: yes -- valid JWT required
+ * Expects: 
+ *    1) user _id from valid JWT
  * Returns: JSON array of portfolio objects
 */
 router.get('/portfolios', (req, res, next) => {
@@ -59,29 +39,45 @@ router.get('/portfolios', (req, res, next) => {
 /**
  * Get a specific portfolio belonging to a user
  * Example: GET >> /api/portfolios/65a4sd654asd645asd
- * Secured: yes
- * Expects: portfolio _id from req params, user _id from JWT
+ * Secured: yes -- valid JWT required
+ * Expects: 
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
  * Returns: JSON portfolio objects
 */
 router.get('/portfolios/:id', (req, res, next) => {
+
+  if (!isOid(req.params.id)) {
+    return res.status(400).json({ message: badParamMsg});
+  }
+
   Portfolios.getOne(req.user._id, req.params.id)
     .then(portfolio => res.status(200).json(portfolio))
     .catch(err => next(err));
+
 });
 
 
 /**
  * Create a new portfolio
  * Example: POST >> /api/portfolios
- * Secured: yes
- * Expects: portfolio name from req body, user _id from JWT
+ * Secured: yes -- valid JWT required
+ *    1) user _id from JWT
+ *    2) portfolio name from req body
  * Returns: JSON portfolio object
 */
 router.post('/portfolios', (req, res, next) => {
+
+  if (!req.body.name) {
+    return res.status(400).json({ message: 'Portfolio "name" is required.' });
+  }
+  
+  const notes = req.body.notes || '';
+
   const newPortfolio = {
     owner : req.user._id,
     name  : req.body.name,
-    notes : req.body.notes
+    notes
   };
   
   Portfolios.create(newPortfolio)
@@ -94,15 +90,24 @@ router.post('/portfolios', (req, res, next) => {
 /**
  * Update a user's portfolio
  * Example: PUT >> /api/portfolios/564asd654asd56a4sd
- * Secured: yes
- * Expects: portfolio _id from req params, updates from req body, user _id from JWT
+ * Secured: yes -- valid JWT required
+ * Expects:
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
+ *    3) updates from req body
  * Returns: JSON portfolio object
 */
 router.put('/portfolios/:id', (req, res, next) => {
+
+  if (!isOid(req.params.id)) {
+    return res.status(400).json({ message: badParamMsg });
+  }
+
   const filter = {
     owner : req.user._id,
     _id   : req.params.id
   };
+  
   const updates = {
     name  : req.body.name,
     notes : req.body.notes
@@ -118,11 +123,17 @@ router.put('/portfolios/:id', (req, res, next) => {
 /**
  * Delete a user's portfolio
  * Example: DELETE >> /api/portfolios/564asd654asd56a4sd
- * Secured: yes
- * Expects: portfolio _id from req params, user _id from valid JWT
+ * Secured: yes -- valid JWT required
+ * Expects: 
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
  * Returns: JSON success message
 */
 router.delete('/portfolios/:id', (req, res, next) => {
+
+  if (!isOid(req.params.id)) {
+    return res.status(400).json({ message: badParamMsg });
+  }
 
   Portfolios.deletePortfolio(req.user._id, req.params._id)
     .then(result => res.status(200).json(result))
@@ -134,21 +145,46 @@ router.delete('/portfolios/:id', (req, res, next) => {
 /**
  * Add holding to portfolio
  * Example: POST >> /api/portfolios/564asd654asd56a4sd/holdings
- * Secured: yes
- * Expects: portfolio _id from req params, ticker and qty from req body, username and _id from valid JWT
+ * Secured: yes -- valid JWT required
+ * Expects:
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
+ *    3) ticker and qty from req body
  * Returns: JSON portfolio object
 */
-router.post('/portfolios/:id/holdings', (req, res, next) => {
+router.post('/portfolios/:id/holdings', async (req, res, next) => {
 
-  const filter = {
-    owner  : req.user._id,
-    _id    : req.params.id
-  };
-  
-  const updates = {
-    ticker : req.body.ticker,
-    qty    : req.body.qty
-  };
+  if (!req.body.ticker || !req.body.qty) {
+    return res.status(400)
+      .json({ message: 'Missing required holding details.' });
+  }
+
+  if (Number.isNaN(+req.body.qty)) {
+    return res.status(400).json({ message: 'Qty must be a number.' });
+  }
+
+  if (!isOid(req.params.id)) {
+    return res.status(400).json({ message: badParamMsg });
+  }
+
+  const owner  = req.user._id;
+  const _id    = req.params.id;
+  const ticker = req.body.ticker.toString().trim().toUpperCase();
+  const qty    = +req.body.qty;  // coerse to number type
+
+  if (!isTicker(ticker)) {
+    return res.status(400).json({ message: badBodyMsg });
+  }
+
+  const filter  = { owner, _id };
+  const updates = { ticker, qty };
+
+  // check if holding already exists
+  const holding = await Portfolios.hasHolding(owner, _id, ticker);
+  if (holding) {
+    return res.status(403)
+      .json({message: `Holding ${ticker} already exists in portfolio.`});
+  }
 
   Portfolios.addHolding(filter, updates)
     .then(result => res.status(200).json(result))
@@ -159,25 +195,70 @@ router.post('/portfolios/:id/holdings', (req, res, next) => {
 
 /**
  * Update a holding in a user's portfolio
- * Example: PUT >> /api/portfolios/564asd654asd56a4sd/holdings/65asd65a4sd564asd564
- * Secured: yes
- * Expects: portfolio _id & holding _id from req params, qty from req body, username and _id from valid JWT
+ * Example: PUT >> /api/portfolios/564asd654asd54sd/holdings/65asd64sd564asd564
+ * Secured: yes -- valid JWT required
+ * Expects: 
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
+ *    3) holding _id from req params
+ *    4) qty from req body
  * Returns: JSON portfolio object
 */
-router.put('/portfolios/:id/holdings/:id', (req, res) => {
-  res.status(200).json({ message: 'router is working OK!' });
+router.put('/portfolios/:pfloId/holdings/:hldgId', (req, res, next) => {
+
+  if (!isOid(req.params.pfloId || !isOid(req.params.hldgId))) {
+    return res.status(400).json({ message: badParamMsg });
+  }
+
+  if (!req.body.qty) {
+    return res.status(400).json({ message: 'Missing holding quantity.' });
+  }
+
+  if (Number.isNaN(+req.body.qty)) {
+    return res.status(400).json({ message: 'Qty must be a number.' });
+  }
+
+  const filter = {
+    owner  : req.user._id,
+    pfloId : req.params.pfloId,
+    hldgId : req.params.hldgId
+  };
+
+  const qty = +req.body.qty;  // coerce to number type
+
+  Portfolios.updateHolding(filter, qty)
+    .then(result => res.status(200).json(result))
+    .catch(err => next(err));
+
 });
 
 
 /**
  * Delete a holding from a user's portfolio
- * Example: DELETE >> /api/portfolios/564asd654asd56a4sd/holdings/65asd65a4sd564asd564
- * Secured: yes
- * Expects: portfolio _id & holding _id from req params, username and _id from valid JWT
+ * Example: DELETE >> /api/portfolios/564asd654asd4sd/holdings/65asd4sd564asd564
+ * Secured: yes -- valid JWT required
+ * Expects: 
+ *    1) user _id from JWT
+ *    2) portfolio _id from req params
+ *    3) holding _id from req params
  * Returns: JSON success message
 */
-router.delete('/portfolios/:id/holdings/:id', (req, res) => {
-  res.status(200).json({ message: 'router is working OK!' });
+router.delete('/portfolios/:pfloId/holdings/:hldgId', (req, res, next) => {
+
+  if (!isOid(req.params.pfloId || !isOid(req.params.hldgId))) {
+    return res.status(400).json({ message: badParamMsg });
+  }
+
+  const target = {
+    owner  : req.user._id,
+    pfloId : req.params.pfloId,
+    hldgId : req.params.hldgId
+  };
+
+  Portfolios.deleteHolding(target)
+    .then(result => res.status(200).json(result))
+    .catch(err => next(err));
+
 });
 
 
