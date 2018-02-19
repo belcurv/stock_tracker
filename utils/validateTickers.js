36e54a74022d035b4da3d490528b287d1551b73c
Jay Schwane <jrschwane@uwalumni.com>
2018-02-19 00:51:54 -0600

    Adds utility to validate shape of tickers

---
+++ utils/validateTickers.js
@@ -0,0 +1,10 @@
+/**
+ * Validate stock ticker strings.
+ * Assumes no more than 5 uppercase, alpha-numeric characters.
+ * @param     {String}   ticker   Input ticker
+ * @returns   {Boolean}           True if ticker matches pattern
+*/
+module.exports = (ticker) => {
+  const rex = /^[0-9A-Z]{5}$/;
+  return typeof ticker === 'string' && rex.test(ticker);
+};
