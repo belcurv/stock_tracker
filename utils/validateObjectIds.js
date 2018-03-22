5ab7f829f3358fdcadf2fcd8e6ecda6768c377e3
Jay Schwane <jrschwane@uwalumni.com>
2018-03-22 10:10:46 -0500

    Reformats comments

--- utils/validateObjectIds.js
+++ utils/validateObjectIds.js
@@ -1,13 +1,11 @@
-/**
- * Validates that a paramter is a valid MongoDB ObjectID.
- * A valid MongoDB ObjectID is 12 hex bytes, aka
- * 24 alphanumeric chars 0-9 and a-f.
- * @param    {String}   _id          Candidate _id to check
- * @param    {String}   schemaType   Optional type to add error specificity
- * @returns  {Boolean}               True when _id is valid
- * @throws   {Error}                 Throws when _id is missing or invalid
+/** Validates that a paramter is a valid MongoDB ObjectID.
+ *  A valid MongoDB ObjectID is 12 hex bytes, aka
+ *  24 alphanumeric chars 0-9 and a-f.
+ *  @param    {String}   _id          Candidate _id to check
+ *  @param    {String}   schemaType   Optional type to add error specificity
+ *  @returns  {Boolean}               True when _id is valid
+ *  @throws   {Error}                 Throws when _id is missing or invalid
 */
-
 module.exports = (_id, schemaType) => {
   const rex = /^[0-9a-fA-F]{24}$/;
 
