e35527dc3f9885640e2ac3ff98bb7f1f414c0ff0
Jay Schwane <jrschwane@uwalumni.com>
2018-03-31 08:24:59 -0500

    Adds cors options config file

---
+++ config/cors_options.js
@@ -0,0 +1,14 @@
+/**
+ * CORS configuration object
+ * For options, see: https://github.com/expressjs/cors
+ * 
+ *  ** UPDATE 'origin' ARRAY WHEN REAL CLIENT(S) DEPLOYED
+ */
+module.exports = {
+  origin: [
+    /^http:\/\/localhost(:[0-9]{0,4})?\/?$/
+  ],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  exposedHeaders: ['Access-Control-Allow-Origin'],
+  optionsSuccessStatus: 200,
+};
